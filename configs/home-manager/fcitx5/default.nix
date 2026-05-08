d3b1963d06a054cc636e9829c3b0ae4c606f3331
mst-mkt <mail@keito.dev>
2026-05-09 01:08:45 +0900

    add(fcitx): manage fcitx config as nix

--- configs/home-manager/fcitx5/default.nix
+++ configs/home-manager/fcitx5/default.nix
@@ -10,6 +10,20 @@
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
-    fcitx5.waylandFrontend = true;
+    fcitx5 = {
+      waylandFrontend = true;
+      settings = {
+        inputMethod = {
+          GroupOrder."0" = "Default";
+          "Groups/0" = {
+            Name = "Default";
+            "Default Layout" = "us";
+            DefaultIM = "keyboard-us";
+          };
+          "Groups/0/Items/0".Name = "hazkey";
+          "Groups/0/Items/1".Name = "keyboard-us";
+        };
+      };
+    };
   };
 }
