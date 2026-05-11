f443dc997e7d8cc34a80bb4dccc9c2ced687255d
mst-mkt <mail@keito.dev>
2026-05-12 03:09:19 +0900

    fix(niri): add window-rule for vivaldi popup window (floating, position, no-focusring)

--- configs/home-manager/niri/window-rules.nix
+++ configs/home-manager/niri/window-rules.nix
@@ -6,5 +6,21 @@
       matches = [ { app-id = ''^org\.wezfurlong\.wezterm$''; } ];
       default-column-width.proportion = 0.5;
     }
+    {
+      # Vivaldi popup windows
+      matches = [
+        {
+          app-id = "^$";
+          title = "^$";
+        }
+      ];
+      open-floating = true;
+      default-floating-position = {
+        x = 16;
+        y = 16;
+        relative-to = "bottom-right";
+      };
+      focus-ring.enable = false;
+    }
   ];
 }
