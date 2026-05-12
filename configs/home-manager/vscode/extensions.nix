17d83c143a5b85d9dc2ae9be04e9ff8ff97a0b09
mst-mkt <mail@keito.dev>
2026-05-13 03:04:02 +0900

    add(vscode): some extensions (ibm.output-colorizer, usernamehw.errorlens, yoavbls.pretty-ts-errors, ionutvmi.path-autocomplete)

--- configs/home-manager/vscode/extensions.nix
+++ configs/home-manager/vscode/extensions.nix
@@ -5,6 +5,7 @@
     (with pkgs.vscode-extensions; [
       charliermarsh.ruff
       github.copilot-chat
+      ibm.output-colorizer
       jnoortheen.nix-ide
       mkhl.direnv
       ms-python.python
@@ -13,9 +14,12 @@
       streetsidesoftware.code-spell-checker
       tamasfe.even-better-toml
       thenuprojectcontributors.vscode-nushell-lang
+      usernamehw.errorlens
       vitest.explorer
+      yoavbls.pretty-ts-errors
     ])
     ++ (with pkgs.vscode-marketplace-release-universal; [
+      ionutvmi.path-autocomplete
       oxc.oxc-vscode
       thang-nm.flow-icons
     ]);
