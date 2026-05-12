4e234d4ee3e0ab4a2157476be88ffcf1d6b987ba
mst-mkt <mail@keito.dev>
2026-05-12 23:59:07 +0900

    add(vscode): editor.* settings

--- configs/home-manager/vscode/settings.nix
+++ configs/home-manager/vscode/settings.nix
@@ -1,9 +1,48 @@
 {
   programs.vscode.profiles.default.userSettings = {
+    # editor.*
+    "editor.fontFamily" = "'UDEV Gothic NF', monospace";
+    "editor.fontSize" = 12;
+    "editor.fontLigatures" = true;
+    "editor.tabSize" = 2;
+    "editor.insertSpaces" = true;
+    "editor.detectIndentation" = false;
+    "editor.formatOnSave" = true;
+    "editor.minimap.enabled" = false;
+    "editor.renderWhitespace" = "all";
+    "editor.rulers" = [
+      80
+      100
+    ];
+    "editor.stickyScroll.enabled" = false;
+    "editor.guides.bracketPairs" = true;
+    "editor.guides.bracketPairsHorizontal" = true;
+    "editor.bracketPairColorization.independentColorPoolPerBracketType" = true;
+    "editor.renderLineHighlight" = "gutter";
+    "editor.cursorBlinking" = "phase";
+    "editor.cursorStyle" = "line";
+    "editor.cursorWidth" = 3;
+    "editor.cursorSmoothCaretAnimation" = "explicit";
+    "editor.smoothScrolling" = true;
+    "editor.scrollbar.vertical" = "visible";
+    "editor.scrollbar.verticalScrollbarSize" = 8;
+    "editor.scrollbar.horizontalScrollbarSize" = 4;
+    "editor.scrollbar.scrollByPage" = true;
+    "editor.tabCompletion" = "on";
+    "editor.suggest.preview" = true;
+    "editor.linkedEditing" = true;
+    "editor.multiCursorModifier" = "ctrlCmd";
+    "editor.dragAndDrop" = false;
+    "editor.copyWithSyntaxHighlighting" = false;
+    "editor.wordSeparators" = "`~!@#$%^&*()-=+[{]}\\|;:'\",.<>/?_（）。、「」・　";
+
+    # workbench.*
     "workbench.startupEditor" = "none";
     "workbench.colorTheme" = "Default Dark Modern";
     "workbench.iconTheme" = "flow-deep";
 
+
+    # github.copilot.*
     "github.copilot.enable" = {
       "*" = true;
     };
