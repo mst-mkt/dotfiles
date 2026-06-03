f09d796417d3fe69de8ec493fcdd11c0fe99bd2a
mst-mkt <mail@keito.dev>
2026-06-03 21:14:01 +0900

    feat(ragenix): enable ragenix for home-manager

--- modules/core/secrets.nix
+++ modules/core/secrets.nix
@@ -11,6 +11,7 @@ delib.module {
   options = delib.singleEnableOption true;
 
   nixos.always.imports = [ inputs.ragenix.nixosModules.default ];
+  home.always.imports = [ inputs.ragenix.homeManagerModules.default ];
 
   nixos.ifEnabled = {
     age.identityPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
