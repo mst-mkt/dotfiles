c67f4152a208353934e20658ec8c062335818aa9
mst-mkt <mail@keito.dev>
2026-08-02 06:55:00 +0900

    add: mst-mkt cachix substituter

--- modules/core/nix.nix
+++ modules/core/nix.nix
@@ -22,6 +22,7 @@ delib.module {
         "https://nix-community.cachix.org"
         "https://niri.cachix.org"
         "https://moocs-collect-nix.cachix.org"
+        "https://mst-mkt.cachix.org"
         "https://cache.numtide.com"
       ];
       trusted-public-keys = [
@@ -29,6 +30,7 @@ delib.module {
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
         "moocs-collect-nix.cachix.org-1:MpREl4nnQpusRFLilrWt2S67SW1mLuqM6HvcirF/CjE="
+        "mst-mkt.cachix.org-1:Ap1WSTd2tPEsFkNutQ7+X8OGtv7kOy9Q+xzvBvcL7FU="
         "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
       ];
     };
