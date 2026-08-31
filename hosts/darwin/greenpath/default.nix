4fd15599cf47797940283fc436d041c99d68dd27
mst-mkt <mst-mkt@hatena.com>
2026-08-31 16:01:45 +0900

    feat(hosts): add darwin host "greenpath"

---
+++ hosts/darwin/greenpath/default.nix
@@ -0,0 +1,16 @@
+{ delib, ... }:
+
+delib.host {
+  name = "greenpath";
+  type = "laptop";
+  usage = "work";
+  owner = "mst-mkt";
+  system = "aarch64-darwin";
+  features = [
+    "dev"
+    "gui"
+  ];
+
+  darwin.system.stateVersion = 7;
+  home.home.stateVersion = "25.11";
+}
