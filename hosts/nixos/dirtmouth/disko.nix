0f00456d124e954da69c8bab9160bc93e15ace1d
mst-mkt <mail@keito.dev>
2026-09-01 00:39:04 +0900

    fix(dirtmouth): replace hardware fileSystems with disko layout

---
+++ hosts/nixos/dirtmouth/disko.nix
@@ -0,0 +1,69 @@
+{ delib, ... }:
+
+delib.host {
+  name = "dirtmouth";
+
+  nixos.disko.devices.disk.main = {
+    type = "disk";
+    device = "/dev/nvme0n1";
+    content = {
+      type = "gpt";
+      partitions = {
+        ESP = {
+          priority = 1;
+          label = "ESP";
+          size = "512M";
+          type = "EF00";
+          content = {
+            type = "filesystem";
+            format = "vfat";
+            mountpoint = "/boot";
+            mountOptions = [
+              "fmask=0077"
+              "dmask=0077"
+            ];
+          };
+        };
+        swap = {
+          priority = 2;
+          label = "swap";
+          size = "8G";
+          content = {
+            type = "swap";
+          };
+        };
+        root = {
+          priority = 3;
+          label = "root";
+          size = "100%";
+          content = {
+            type = "btrfs";
+            subvolumes = {
+              "/root" = {
+                mountpoint = "/";
+                mountOptions = [
+                  "compress=zstd"
+                  "noatime"
+                ];
+              };
+              "/home" = {
+                mountpoint = "/home";
+                mountOptions = [
+                  "compress=zstd"
+                  "noatime"
+                ];
+              };
+              "/nix" = {
+                mountpoint = "/nix";
+                mountOptions = [
+                  "compress=zstd"
+                  "noatime"
+                ];
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
