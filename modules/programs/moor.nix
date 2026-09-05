80ebbe2e9c2b58bbb89b221008ee25f1bdb2b7c2
mst-mkt <mail@keito.dev>
2026-09-05 23:49:17 +0900

    add(moor): moor as the default pager

---
+++ modules/programs/moor.nix
@@ -0,0 +1,33 @@
+{
+  delib,
+  host,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  moorOptions = {
+    quit-if-one-screen = true;
+    no-clear-on-exit = true;
+    wrap = true;
+    tab-size = 4;
+  };
+
+  env = {
+    MOOR = lib.cli.toGNUCommandLineShell { } moorOptions;
+    PAGER = "moor --no-linenumbers";
+  };
+in
+
+delib.module {
+  name = "programs.moor";
+
+  options = delib.singleEnableOption host.cliFeatured;
+
+  home.ifEnabled = {
+    home.packages = [ pkgs.moor ];
+    home.sessionVariables = env;
+    programs.nushell.environmentVariables = env;
+  };
+}
