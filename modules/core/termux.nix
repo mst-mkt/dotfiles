f9aee7ed43026245a93a8864708a2ac4be7e0941
mst-mkt <mail@keito.dev>
2026-09-05 23:05:39 +0900

    fix(termux): install termux.properties as a regular file

--- modules/core/termux.nix
+++ modules/core/termux.nix
@@ -3,6 +3,7 @@
   host,
   lib,
   pkgs,
+  config,
   ...
 }:
 
@@ -10,53 +11,6 @@ delib.module {
   name = "core.termux";
 
   options = delib.singleEnableOption host.isMobile;
-
-  home.ifEnabled = {
-    home.file.".termux/termux.properties".text = lib.generators.toKeyValue { } {
-      disable-file-share-receiver = true;
-      disable-file-view-receiver = true;
-      disable-terminal-session-change-toast = true;
-      enforce-char-based-input = true;
-
-      extra-keys = builtins.toJSON [
-        [
-          "ESC"
-          "/"
-          ":"
-          ";"
-          "-"
-          "_"
-          "KEYBOARD"
-          "PASTE"
-        ]
-        [
-          "TAB"
-          "HOME"
-          "END"
-          "PGUP"
-          "PGDN"
-          "UP"
-          "|"
-          "~"
-        ]
-        [
-          "CTRL"
-          "ALT"
-          "SHIFT"
-          "BKSP"
-          "LEFT"
-          "DOWN"
-          "RIGHT"
-          "ENTER"
-        ]
-      ];
-
-      terminal-cursor-style = "bar";
-      terminal-margin-horizontal = 8;
-      terminal-onclick-url-open = true;
-      terminal-transcript-rows = 10000;
-    };
-  };
 
   droid.ifEnabled = {
     terminal.font = "${pkgs.udev-gothic-nf}/share/fonts/truetype/UDEVGothicNFLG-Regular.ttf";
@@ -68,5 +22,59 @@ delib.module {
       termux-reload-settings.enable = true;
       xdg-open.enable = true;
     };
+
+    build.activation.installTermuxProperties =
+      let
+        properties = pkgs.writeText "termux.properties" (
+          lib.generators.toKeyValue { } {
+            disable-file-share-receiver = true;
+            disable-file-view-receiver = true;
+            disable-terminal-session-change-toast = true;
+            enforce-char-based-input = true;
+
+            extra-keys = builtins.toJSON [
+              [
+                "ESC"
+                "/"
+                ":"
+                ";"
+                "-"
+                "_"
+                "KEYBOARD"
+                "PASTE"
+              ]
+              [
+                "TAB"
+                "HOME"
+                "END"
+                "PGUP"
+                "PGDN"
+                "UP"
+                "|"
+                "~"
+              ]
+              [
+                "CTRL"
+                "ALT"
+                "SHIFT"
+                "BKSP"
+                "LEFT"
+                "DOWN"
+                "RIGHT"
+                "ENTER"
+              ]
+            ];
+
+            terminal-cursor-style = "bar";
+            terminal-margin-horizontal = 8;
+            terminal-onclick-url-open = true;
+            terminal-transcript-rows = 10000;
+          }
+        );
+      in
+      ''
+        $DRY_RUN_CMD mkdir $VERBOSE_ARG -p "${config.user.home}/.termux"
+        $DRY_RUN_CMD install $VERBOSE_ARG -m 0644 "${properties}" "${config.user.home}/.termux/termux.properties"
+      '';
   };
 }
